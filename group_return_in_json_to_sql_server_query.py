e181f10761090824a221db5a0851bacc412cd791
Sadir Cardoso Junior <44247363+sadircardosojr@users.noreply.github.com>
2021-08-11 11:38:42 -0300

    Create group_return_in_json_to_sql_server_query.py

---
+++ group_return_in_json_to_sql_server_query.py
@@ -0,0 +1,40 @@
+import pyodbc as pyodbc
+
+def getData():
+    
+    server = "YOUR_SERVER"
+    database = "YOUR_DB"
+    username = "YOUR_USER_NAME"
+    password = "YOUR_PASSWORD"
+
+    print('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD=********')
+
+    cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='
+        +server+
+        ';DATABASE='
+        +database+
+        ';UID='
+        +username+
+        ';PWD='
+        + password)
+
+    query = """
+    select  * from YOUR_TABLE 
+    FOR JSON AUTO
+    """
+    
+    cnxn.timeout = 15       
+    cursor = cnxn.cursor()
+    cursor.execute(query)
+    
+    return_query = ""
+
+    for row in cursor:
+        return_query = return_query + str(row[0])
+        
+    return return_query
+
+if __name__ == '__main__':
+    retorno = str(getData())
+
+    print("\n RETORNO: " + retorno)
